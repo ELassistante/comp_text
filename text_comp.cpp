9b0142e8c855a3656494f809f7950d848642cb9b
ELassistante <83674114+ELassistante@users.noreply.github.com>
2022-03-10 21:04:27 +0100

    Initial commit

---
+++ text_comp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std ;
+
+const string message("Line ") ;
+
+int main () {
+
+    string file_name_1 ;
+    string file_name_2 ;
+    string ref ;
+    string comp ;
+    size_t cnt(0) ;
+
+    cout << "Name of the reference file (include extention) :" << endl ;
+    cin >> file_name_1 ;
+    cout << "Name of the file to compare (include extention) :" << endl ;
+    cin >> file_name_2 ; 
+
+    ifstream text_ref ;
+    ifstream text_comp ;
+    ofstream diff ;
+
+    text_ref.open("test_1.txt") ;
+    text_comp.open("test_2.txt") ;
+    diff.open("differences.txt") ;
+
+    while(getline(text_ref >> ws, ref)) {
+        if(getline(text_comp >> ws, comp)) {
+            cnt++ ;
+            if (ref != comp) diff << message << cnt <<  endl ;
+        } else return 0 ;
+    }
+
+    return 0 ;
+}
